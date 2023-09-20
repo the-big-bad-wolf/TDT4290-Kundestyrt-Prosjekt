152dbf9cca69b52e0b967d4093ca38d89b55f333
elisakiv <elis.k.iver@gmail.com>
2023-09-21 00:03:51 +0200

    #5 console.log text in file being worked on

--- Typescript/extension/out/extension.js
+++ Typescript/extension/out/extension.js
@@ -16,8 +16,13 @@ function activate(context) {
     let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from Extension!');
+        vscode.window.showInformationMessage('Now logging changes in open file');
     });
+    //When the text in the document is changed
+    //console.log() the whole document
+    vscode.workspace.onDidChangeTextDocument(event => {
+        console.log(event.document.getText());
+    }, null, context.subscriptions);
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
--- Typescript/extension/src/extension.ts
+++ Typescript/extension/src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+ 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "extension" is now active!');
@@ -16,11 +16,19 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Extension!');
+		vscode.window.showInformationMessage('Now logging changes in open file');
 	});
+	
+	//When the text in the document is changed
+	//console.log() the whole document
+	vscode.workspace.onDidChangeTextDocument(event => {
+    	console.log(event.document.getText!());	
+	}, null, context.subscriptions);
+	
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
