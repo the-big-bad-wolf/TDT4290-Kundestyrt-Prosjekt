5a0c4bb4a50ab8450aae8bc601b7bbef57117533
Mikkel <mikkel@enable.no>
2023-09-21 13:30:44 +0200

    #8 standarize data

--- Python/SimpleARIMAForecasting.py
+++ Python/SimpleARIMAForecasting.py
@@ -2,6 +2,19 @@ import numpy as np
 from statsmodels.tsa.arima.model import ARIMA
 
 
+def standardize_data(data_array):
+    """
+    Standardizes a given 1D-array.
+
+    Parameters:
+    - data_array: A 1D numpy array
+
+    Returns:
+    - standardized_data: A 1D numpy array with a mean of 0 and a standard deviation of 1
+    """
+    return (np.mean(data_array)) / np.std(data_array)
+
+
 def establish_reference(data_array):
     """
     Calculate the average for a given 1D-array.
@@ -10,24 +23,30 @@ def establish_reference(data_array):
     - data_array: A 1D numpy array
 
     Returns:
-    - average: Average of the array
+    - average: Average of the standardized array
     """
-    return np.mean(data_array)
+    return np.mean(data_array), np.std(data_array)
 
 
-def predict_next_direction(data_array, reference_avg):
+def predict_next_direction(data_array, mean, std):
     """
     Predicts the next value and determines its direction (up or down) compared to the reference average.
 
     Parameters:
-    - data_array: A recent 1D numpy array (90 data points each second from the Tobii glasses)
-    - reference_avg: A previously calculated average (from the first 10,800 data points)
+    - data_array: A recent 1D numpy array (120 data points each second from the Tobii glasses)
+    - reference_avg: A previously calculated average (from the first 15000 data points, est. 2 minutes)
 
     Returns:
     - direction: "up" or "down" based on the comparison of forecasted value to reference_avg
     """
-    model = ARIMA(data_array, order=(5, 1, 0))
+
+    z_score_array = (data_array - mean) / std
+
+    model = ARIMA(z_score_array, order=(5, 1, 0))
     model_fit = model.fit()
     forecast = model_fit.forecast(steps=1)[0]
+    print("forecast", forecast)
 
-    return "up" if forecast > reference_avg+2 else "down"
+    return (
+        "up" if np.abs(forecast) > 2 else "down"
+    )  # assuming 0 is mean and 2 is 2 std above
