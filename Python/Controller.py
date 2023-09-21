a734d7470209d5fff18679b2fdc1759f45449f92
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2023-09-21 02:21:42 +0200

    #1 print cognitive load

--- Python/Controller.py
+++ Python/Controller.py
@@ -1,4 +1,6 @@
 import tobii_research as tr
+from CognitiveLoad import compute_cognitive_load
+from math import isnan
 
 found_eyetrackers = tr.find_all_eyetrackers()
 my_eyetracker = found_eyetrackers[0]
@@ -8,15 +10,30 @@ print("Name (It's OK if this is empty): " + my_eyetracker.device_name)
 print("Serial number: " + my_eyetracker.serial_number)
 
 
-def callback(gaze_data):
-    # Print gaze points of left and right eye
-    print(
-        "Left eye: ({gaze_left_eye}) \t Right eye: ({gaze_right_eye})".format(
-            gaze_left_eye=gaze_data["left_gaze_point_on_display_area"],
-            gaze_right_eye=gaze_data["right_gaze_point_on_display_area"],
-        )
-    )
 
+valid_lpup = [0.5]
+valid_rpup = [0.5]
+def callback(gaze_data):
+    # Print pupil size of left and right eye
+    lpup = gaze_data['left_pupil_diameter']
+    rpup = gaze_data['right_pupil_diameter']
+
+    """If pupil data is invalid, use other valid pupil or the previous valid pupil data"""
+    if isnan(lpup) and isnan(rpup):
+        valid_lpup.append(valid_lpup[-1])
+        valid_rpup.append(valid_rpup[-1])
+    elif isnan(lpup) and not isnan(rpup):
+        valid_lpup.append(rpup)
+        valid_rpup.append(rpup)
+    elif not isnan(lpup) and isnan(rpup):
+        valid_lpup.append(lpup)
+        valid_rpup.append(lpup)
+    else:
+        valid_lpup.append(lpup)
+        valid_rpup.append(rpup)
+
+    cognitive_load = compute_cognitive_load(valid_lpup, valid_rpup)
+    print(cognitive_load)
 
 my_eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, callback, as_dictionary=True)
 
