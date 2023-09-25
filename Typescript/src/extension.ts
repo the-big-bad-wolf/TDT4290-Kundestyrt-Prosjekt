d11abea61a286525010d59b69875ebe67f4ed325
elisakiv <elis.k.iver@gmail.com>
2023-09-25 15:43:39 +0200

    #14 simple logging of code to csv

--- Typescript/src/extension.ts
+++ Typescript/src/extension.ts
@@ -2,40 +2,54 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { fetchData } from "./listener";
+import * as fs from "fs";
+import * as path from "path";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "extension" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "extension" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand("extension.showData", async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const message = await fetchData();
-		if (message) {
-			vscode.window.showInformationMessage(message);
-		} else {
-			vscode.window.showErrorMessage("Failed to fetch data from Server.py.");
-		}
-		vscode.window.showInformationMessage("Now logging changes in open file");
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "extension.showData",
+    async () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      const message = await fetchData();
+      if (message) {
+        vscode.window.showInformationMessage(message);
+      } else {
+        vscode.window.showErrorMessage("Failed to fetch data from Server.py.");
+      }
+      vscode.window.showInformationMessage("Now logging changes in open file");
+    }
+  );
 
-	//When the text in the document is changed
-	//console.log() the whole document
-	vscode.workspace.onDidChangeTextDocument(
-		event => {
-			console.log(event.document.getText!());
-		},
-		null,
-		context.subscriptions
-	);
+  //logs the code in open file to csv file every five seconds
+  //along with the time
+  setInterval(() => {
+    const editor = vscode.window.activeTextEditor;
+    const highlighted = editor!.document.getText();
+    const now = new Date().toLocaleString();
 
-	context.subscriptions.push(disposable);
+    const data = `${now},\n${highlighted}\n`;
+
+    try {
+      fs.appendFileSync(
+        path.join(__dirname, "../src/vscodeData.csv"),
+        data + "\n\n"
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  }, 5000);
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
