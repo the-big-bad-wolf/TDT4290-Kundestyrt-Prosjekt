9bcc354e2d87342e80304899afca6374ec03d43a
elisakiv <elis.k.iver@gmail.com>
2023-09-28 15:40:57 +0200

    #19 simple method of displaying data in stausbar

--- Typescript/src/listener.ts
+++ Typescript/src/listener.ts
@@ -1,7 +1,17 @@
 import * as WebSocket from "ws";
+import * as vscode from "vscode";
 
-export function setUp() {
+let statusBarItem: vscode.StatusBarItem;
+
+export function setUp(context: vscode.ExtensionContext) {
   const ws = new WebSocket("ws://localhost:8080");
+
+  statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+  statusBarItem.text = "waiting for data";
+  statusBarItem.show();
 
   ws.on("error", console.error);
 
@@ -11,5 +21,12 @@ export function setUp() {
 
   ws.on("message", function message(data) {
     console.log("received: %s", data);
+
+    let outputJson = JSON.parse(data.toString());
+
+    statusBarItem.text =
+      "cognitive load: " + outputJson["Current cognitive load"];
+
+    context.subscriptions.push(statusBarItem);
   });
 }
