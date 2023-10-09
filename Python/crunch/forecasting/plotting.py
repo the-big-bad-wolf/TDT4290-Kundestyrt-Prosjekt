1405bcdbc6e686038643d51c9412c7e0db2730c3
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2023-10-09 15:42:49 +0200

    # fix mse

--- Python/crunch/forecasting/plotting.py
+++ Python/crunch/forecasting/plotting.py
@@ -5,69 +5,77 @@ import warnings
 import matplotlib.pyplot as plt
 from crunch.forecasting.arma import ARMAClass
 
+
 class Plotting:
-	def __init__(self):
-		self.old_forecast=None
-		self.squared_errors=[]
-		self.MSEs=[]
-		self.fig, (self.ax, self.mse_ax) = plt.subplots(2, 1, figsize=(10, 12))
-		plt.ion()
-		
-	def plot(self, standardized_data, forecast):
-		"""
-		TODO sjekk om denne oppdateres
+    def __init__(self):
+        self.old_forecast = None
+        self.squared_errors = []
+        self.MSEs = []
+        self.fig, (self.ax, self.mse_ax) = plt.subplots(2, 1, figsize=(10, 12))
+        plt.ion()
 
-		Args:
-			new_value (_type_): _description_
-			forecast (_type_): _description_
-		"""
-		if not hasattr(self, "fig"):
-			self.fig, self.ax = plt.subplots(figsize=(10, 6))
-			plt.ion()  # Aktiver interaktiv modus
+    def plot(self, standardized_data, forecast):
+        """
+        TODO sjekk om denne oppdateres
 
-		self.ax.clear()  # Fjern tidligere plott
+        Args:
+                new_value (_type_): _description_
+                forecast (_type_): _description_
+        """
+        if not hasattr(self, "fig"):
+            self.fig, self.ax = plt.subplots(figsize=(10, 6))
+            plt.ion()  # Aktiver interaktiv modus
 
-		# Plot the data
-		self.ax.plot(standardized_data, label="Data", color="blue")
+        self.ax.clear()  # Fjern tidligere plott
 
-		# Plot the new value
-		self.ax.scatter(
-			len(standardized_data) - 1, standardized_data[-1], color="red", label="New Value"
-		)
+        # Plot the data
+        self.ax.plot(standardized_data, label="Data", color="blue")
 
-		# Plot the forecast
-		forecast_x_values = np.arange(len(standardized_data), len(standardized_data) + len(forecast))     
-		self.ax.plot(forecast_x_values, forecast, color="green", label="Forecast", linestyle='--')
-		self.ax.set_ylim(-5,5)
+        # Plot the new value
+        self.ax.scatter(
+            len(standardized_data) - 1,
+            standardized_data[-1],
+            color="red",
+            label="New Value",
+        )
 
-		self.ax.set_title("Cognitive Load Data and Forecast")
-		self.ax.set_xlabel("Time")
-		self.ax.set_ylabel("Standardized Value")
-		self.ax.legend()
-		self.ax.grid(True)
+        # Plot the forecast
+        forecast_x_values = np.arange(
+            len(standardized_data), len(standardized_data) + len(forecast)
+        )
+        self.ax.plot(
+            forecast_x_values, forecast, color="green", label="Forecast", linestyle="--"
+        )
+        self.ax.set_ylim(-5, 5)
 
-		plt.draw()  # Oppdater figuren
-		plt.pause(1)  # Legg til en liten forsinkelse'
+        self.ax.set_title("Cognitive Load Data and Forecast")
+        self.ax.set_xlabel("Time")
+        self.ax.set_ylabel("Standardized Value")
+        self.ax.legend()
+        self.ax.grid(True)
 
-	def backtest(self,new_value,forecast):
-		if self.old_forecast==None:
-			self.old_forecast=new_value
-			return
-		squared_error=(new_value-self.old_forecast)**2
-		self.old_forecast=forecast
-		self.squared_errors.append(squared_error)
-		MSE=np.mean(self.squared_errors)/len(self.squared_errors)
-		self.MSEs.append(MSE)
-		self._plot_mse()
+        plt.draw()  # Oppdater figuren
+        plt.pause(1)  # Legg til en liten forsinkelse'
 
-	def _plot_mse(self):         
-		self.mse_ax.clear()
-		self.mse_ax.plot(self.MSEs, label="MSE", color="red")
+    def backtest(self, new_value, forecast):
+        if self.old_forecast == None:
+            self.old_forecast = new_value
+            return
+        squared_error = (new_value - self.old_forecast) ** 2
+        self.old_forecast = forecast
+        self.squared_errors.append(squared_error)
+        MSE = np.mean(self.squared_errors)
+        self.MSEs.append(MSE)
+        self._plot_mse()
 
-		self.mse_ax.set_title("Mean Squared Error Over Time")
-		self.mse_ax.set_xlabel("Number of Predictions")
-		self.mse_ax.set_ylabel("MSE")
-		self.mse_ax.legend()
-		self.mse_ax.grid(True)
-		plt.draw()
-		plt.pause(1)
+    def _plot_mse(self):
+        self.mse_ax.clear()
+        self.mse_ax.plot(self.MSEs, label="MSE", color="red")
+
+        self.mse_ax.set_title("Mean Squared Error Over Time")
+        self.mse_ax.set_xlabel("Number of Predictions")
+        self.mse_ax.set_ylabel("MSE")
+        self.mse_ax.legend()
+        self.mse_ax.grid(True)
+        plt.draw()
+        plt.pause(1)
