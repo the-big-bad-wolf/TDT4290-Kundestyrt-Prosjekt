a5aabc0e27e5495733f4e1779b327cf50882ad42
Mikkel <mikkel@enable.no>
2023-10-12 17:11:54 +0200

    #35 limited to only plotting last 30 items

--- Python/crunch/forecasting/plotting.py
+++ Python/crunch/forecasting/plotting.py
@@ -25,24 +25,28 @@ class Plotting:
 
         self.ax.clear()  # Fjern tidligere plott
 
+        # Only consider the last 30 items
+        data_to_plot = standardized_data[-30:]
+        averages_to_plot = self.averages[-30:]
+
         # Plot the data
-        self.ax.plot(standardized_data, label="Data", color="blue")
+        self.ax.plot(data_to_plot, label="Data", color="blue")
 
         # Plot the historical average forecast
         self.ax.plot(
-            self.averages, label="Average Forecast", color="purple", linestyle=":"
+            averages_to_plot, label="Average Forecast", color="purple", linestyle=":"
         )
         # Plot the new value
         self.ax.scatter(
-            len(standardized_data) - 1,
-            standardized_data[-1],
+            len(data_to_plot) - 1,
+            data_to_plot[-1],
             color="red",
             label="New Value",
         )
 
-        # Plot the forecast
+        # Adjust the forecast_x_values to start from the current data point
         forecast_x_values = np.arange(
-            len(standardized_data) - 1, len(standardized_data) + len(forecast) - 1
+            len(data_to_plot) - 1, len(data_to_plot) + len(forecast) - 1
         )
         self.ax.plot(
             forecast_x_values, forecast, color="green", label="Forecast", linestyle="--"
@@ -55,47 +59,52 @@ class Plotting:
         self.ax.legend()
         self.ax.grid(True)
 
-        plt.draw()  # Oppdater figuren
-        plt.pause(1)  # Legg til en liten forsinkelse'
+        # Update the x-axis to reflect the actual data points
+        actual_x_ticks = np.arange(
+            len(standardized_data) - len(data_to_plot), len(standardized_data)
+        )
+        tick_interval = max(
+            1, len(data_to_plot) // 6
+        )  # Adjust dynamically based on data length
+        self.ax.set_xticks(np.arange(0, len(data_to_plot), tick_interval))
+        self.ax.set_xticklabels(actual_x_ticks[::tick_interval])
+
+        plt.draw()  # Update figure
+        plt.pause(1)  # Add minor delay to plotting
 
     def backtest(self, new_value, forecast):
         """Add the forecast to the forecast matrix and compute the MSE.
         Calculate the sum of the first column in the forecast matrix and divide by the number of observations
-        After plotting the MSE shift the matrix left and drop the oldest forecast's first value
-        Because it's beeing compared to the actual value
+        After plotting the MSE shift the matrix down and drop the oldest forecast's last value
+        Because it's being compared to the actual value
 
         Args:
             new_value (float: the next actual value
             forecast (np.list): list consisting of the next 10 forecasted values
         """
-        if self.old_forecast == None:
-            self.forecast_matrix[
-                -1
-            ] = forecast  # Add the first forecast to the last row
-            self.old_forecast = new_value
-            self.counter += 1
-            return
+        # Shift all rows down
+        self.forecast_matrix[1:] = self.forecast_matrix[:-1]
+        # Add the new forecast to the top row
+        self.forecast_matrix[0] = forecast
 
         # Compute the average forecast for the next time step
         left_column_sum = np.sum(self.forecast_matrix[:, 0])
-        average_forecast = (
-            left_column_sum / self.counter
-        )  # divide on the number of observations
+        average_forecast = left_column_sum / min(
+            self.counter + 1, 10
+        )  # Use min to handle cases where counter < 10
 
         # Append the average forecast to the averages list
         self.averages.append(average_forecast)
+        print(f"Average forecast: {average_forecast}")
+        print(f"left_column_sum: {left_column_sum}")
+        print(f"matrix: {self.forecast_matrix}")
 
         # Use the average forecast to compute the squared error
-        squared_error = (new_value - average_forecast) ** 2
+        squared_error = (new_value - self.averages[-1]) ** 2
         self.squared_errors.append(squared_error)
         RMSE = np.sqrt(np.mean(self.squared_errors))
         self.RMSEs.append(RMSE)
         self._plot_rmse()
-
-        # Shift the forecasts left and drop the oldest forecast's first value
-        self.forecast_matrix[:, :-1] = self.forecast_matrix[:, 1:]
-        # Add the new forecast to the last row
-        self.forecast_matrix[-1] = forecast
 
         # Increment the counter until the number of predicted steps is reached
         if self.counter < 10:
@@ -103,13 +112,27 @@ class Plotting:
 
     def _plot_rmse(self):
         self.mse_ax.clear()
-        self.mse_ax.plot(self.RMSEs, label="MSE", color="red")
+
+        # Only consider the last 30 RMSE values
+        rmse_to_plot = self.RMSEs[-30:]
+        x_values = np.arange(len(rmse_to_plot))
+
+        self.mse_ax.plot(x_values, rmse_to_plot, label="RMSE", color="red")
 
         self.mse_ax.set_title("Root Mean Squared Error Over Time")
         self.mse_ax.set_xlabel("Number of Predictions")
         self.mse_ax.set_ylabel("RMSE")
         self.mse_ax.legend()
         self.mse_ax.grid(True)
+
+        # Update the x-axis to reflect the actual prediction numbers
+        actual_x_ticks = np.arange(len(self.RMSEs) - len(rmse_to_plot), len(self.RMSEs))
+        tick_interval = max(
+            1, len(rmse_to_plot) // 6
+        )  # Adjust dynamically based on data length
+        self.mse_ax.set_xticks(x_values[::tick_interval])
+        self.mse_ax.set_xticklabels(actual_x_ticks[::tick_interval])
+
         plt.draw()
         plt.pause(1)
 
@@ -138,7 +161,7 @@ if __name__ == "__main__":
     for i in range(num_data_points - 10):
         new_value = data[i]
         forecast = generate_mock_forecast(new_value, 10, noise_scale)
-        plotter.backtest(new_value, forecast)
         plotter.plot(data[: i + 1], forecast)
+        plotter.backtest(new_value, forecast)
 
     plt.show()
