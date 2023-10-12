a153d58fde2bed2565c770dff44828f2f4e588c1
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2023-10-12 15:07:27 +0200

    #34 update requirements and remove unneccessary test

--- Python/tests/unittest/test_eyetracker/test_eyetracker_handler.py
+++ Python/tests/unittest/test_eyetracker/test_eyetracker_handler.py
@@ -2,7 +2,7 @@ import random
 
 import pytest
 
-from crunch.eyetracker.handler import DataHandler, ThresholdDataHandler
+from crunch.eyetracker.handler import DataHandler
 
 
 @pytest.fixture(scope="module")
@@ -12,7 +12,13 @@ def fixation_fixture():
         end = [203, 399, 539, 1219, 1499, 1701, 2044, 2239, 2358, 2940, 3142, 3675]
         fy = [720, 672, 653, 599, 621, 664, 566, 636, 652, 792, 810, 663]
         fx = [1054, 1166, 1087, 1052, 1048, 1069, 717, 856, 821, 527, 559, 938]
-        return {"initTime": init[index % 12], "endTime": end[index % 12], "fx": fx[index % 12], "fy": fy[index % 12]}
+        return {
+            "initTime": init[index % 12],
+            "endTime": end[index % 12],
+            "fx": fx[index % 12],
+            "fy": fy[index % 12],
+        }
+
     return _fixation_fixture_factory
 
 
@@ -24,18 +30,19 @@ def gaze_point_fixture():
     return _gaze_fixture_factory
 
 
-@pytest.mark.parametrize('window, baseline', [(2, 2), (2, 3), (5, 5), (2, 10)])
+@pytest.mark.parametrize("window, baseline", [(2, 2), (2, 3), (5, 5), (2, 10)])
 def test_gaze_handler(gaze_point_fixture, window, baseline):
-    """ Test that the handler uses data points for baseline
-    until it has received enough, and the use them for measurement """
+    """Test that the handler uses data points for baseline
+    until it has received enough, and the use them for measurement"""
     handler = DataHandler(
         measurement_func=lambda lpup, rpup: 1,
         subscribed_to=["lpup", "rpup"],
         window_length=window,
         window_step=window,
-        baseline_length=baseline)
+        baseline_length=baseline,
+    )
 
-    for i in range(window*baseline):
+    for i in range(window * baseline):
         assert handler.baseline == 0
         assert handler.phase_func == handler.baseline_phase
         handler.add_data_point(gaze_point_fixture())
@@ -44,52 +51,22 @@ def test_gaze_handler(gaze_point_fixture, window, baseline):
     assert handler.phase_func == handler.csv_phase
 
 
-@pytest.mark.parametrize('window, baseline', [(2, 2), (2, 3), (5, 5), (2, 10)])
+@pytest.mark.parametrize("window, baseline", [(2, 2), (2, 3), (5, 5), (2, 10)])
 def test_fixation_handler(fixation_fixture, window, baseline):
-    """ Test that the handler uses data points for baseline
-    until it has received enough, and the use them for measurement """
+    """Test that the handler uses data points for baseline
+    until it has received enough, and the use them for measurement"""
     handler = DataHandler(
         measurement_func=lambda initTime, endTime, fx, fy: 1,
         subscribed_to=["initTime", "endTime", "fx", "fy"],
         window_length=window,
         window_step=window,
-        baseline_length=baseline)
+        baseline_length=baseline,
+    )
 
-    for i in range(window*baseline):
+    for i in range(window * baseline):
         assert handler.baseline == 0
         assert handler.phase_func == handler.baseline_phase
         handler.add_data_point(fixation_fixture(i))
 
     assert handler.baseline != 0
     assert handler.phase_func == handler.csv_phase
-
-
-@pytest.mark.parametrize('window, baseline', [(5, 10)])
-def test_fixation_threshold_handler(fixation_fixture, window, baseline):
-    """ Test that the handler uses data points for baseline and threshold
-    until it has received enough, and the use them for measurement """
-    handler = ThresholdDataHandler(
-        measurement_func=lambda initTime, endTime, fx, fy, short_threshold, long_threshold: 1,
-        subscribed_to=["initTime", "endTime", "fx", "fy"],
-        window_length=window,
-        window_step=window,
-        baseline_length=baseline,
-        threshold_length=baseline
-    )
-
-    for i in range(baseline):
-        assert handler.baseline == 0
-        assert handler.phase_func == handler.threshold_phase
-        assert handler.short_threshold is None
-        assert handler.long_threshold is None
-        handler.add_data_point(fixation_fixture(i))
-
-    for i in range(window*baseline):
-        assert handler.baseline == 0
-        assert handler.phase_func == handler._baseline_phase
-        assert handler.short_threshold != 0
-        assert handler.long_threshold != 0
-        handler.add_data_point(fixation_fixture(i))
-
-    assert handler.baseline != 0
-    assert handler.phase_func == handler._csv_phase
