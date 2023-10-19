3b209a7fd34f70b8c62e24f0f5c4e2dce78234b0
bjorneme <bjorneme@stud.ntnu.no>
2023-10-19 14:02:16 +0200

    test: Test Websocket setup.
    #37

---
+++ Typescript/src/test/suite/listener.test.ts
@@ -0,0 +1,17 @@
+import { Server as MockServer } from 'mock-socket';
+import { setUp } from '../../listener';
+
+test('WebSocket message handling', (done) => {
+  const mockServer = new MockServer('ws://localhost:8080');
+  
+  mockServer.on('connection', (socket: any) => {
+    socket.send('Your mock message here');
+  });
+
+  setUp();
+
+  // Assertions related to the expected behavior upon receiving the mock message
+  // ...
+
+  mockServer.stop(done);
+});
