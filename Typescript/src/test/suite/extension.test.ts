e7b725fa792a5ece9821fcb08616e8fc45a4b410
bjorneme <bjorneme@stud.ntnu.no>
2023-10-30 13:16:07 +0100

    feat: Implement test HelpNotification.
    #58

--- Typescript/src/test/suite/extension.test.ts
+++ Typescript/src/test/suite/extension.test.ts
@@ -1,4 +1,4 @@
-import { deactivate, pauseNotification } from "../../extension";
+import { activateCopilotChat, deactivate, offerHelpNotification, pauseNotification, setTimeHelpPropmtWasActivated  } from "../../extension";
 import * as sinon from "sinon";
 import * as vscode from "vscode";
 import * as assert from "assert";
@@ -19,16 +19,55 @@ suite("Extension Test Suite", () => {
   });
 
   test("pauseNotification should show a warning message if the last prompt was more than 2 minutes ago", () => {
+    // Arrange
     const showWarningMessageStub = sandbox.stub(vscode.window, "showWarningMessage");
     const twoMinutesAgo = new Date().getTime() - (2 * 60 * 1000 + 1);
+
+    // Act
     pauseNotification(twoMinutesAgo);
-    assert.strictEqual(showWarningMessageStub.called, true);
+
+    // Assert
+    assert.strictEqual(showWarningMessageStub.called, true, "Expected a warning message to be shown");
   });
 
   test("pauseNotification should not show a warning message if the last prompt was less than 2 minutes ago", () => {
+    // Arrange
     const showWarningMessageStub = sandbox.stub(vscode.window, "showWarningMessage");
     const oneMinuteAgo = new Date().getTime() - (1 * 60 * 1000);
+
+    // Act
     pauseNotification(oneMinuteAgo);
-    assert.strictEqual(showWarningMessageStub.called, false);
+
+    // Assert
+    assert.strictEqual(showWarningMessageStub.called, false, "Expected no warning message to be shown");
+  });
+
+  test("activateCopilotChat should execute the correct command", () => {
+    // Arrange
+    const executeCommandStub = sandbox.stub(vscode.commands, "executeCommand");
+  
+    // Act
+    activateCopilotChat();
+  
+    // Assert
+    assert.strictEqual(executeCommandStub.calledWith("github.copilot.interactiveEditor.explain"), true, "Expected Copilot command to be executed");
+  });
+  
+  test("offerHelpNotification should show a warning message with the correct options", async () => {
+    const yesOption: vscode.MessageItem = { title: "Yes, please" };
+    const noOption: vscode.MessageItem = { title: "No, thank you" };
+    const showWarningMessageStub = sandbox.stub(vscode.window, "showWarningMessage").resolves(yesOption);
+  
+    // Set timeHelpPropmtWasActivated to more than 2 minutes ago
+    setTimeHelpPropmtWasActivated(new Date().getTime() - (2 * 60 * 1000 + 1));
+
+    // Act
+    await offerHelpNotification();
+  
+    // Assert
+    assert.strictEqual(showWarningMessageStub.called, true, "Expected a warning message to be shown");
+    assert.strictEqual(showWarningMessageStub.firstCall.args[0], "Would you like help with your task?");
+    assert.deepStrictEqual(showWarningMessageStub.firstCall.args[1], yesOption.title);
+    assert.deepStrictEqual(showWarningMessageStub.firstCall.args[2], noOption.title);
   });
 });
