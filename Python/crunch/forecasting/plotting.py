8a09a7f5632fab50942959ee70e8bb3668a5aafb
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2023-10-30 13:51:36 +0100

    #55 fix x-ticks

--- Python/crunch/forecasting/plotting.py
+++ Python/crunch/forecasting/plotting.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from crunch import util
-
 
 class Plotting:
     def __init__(self):
@@ -10,12 +8,7 @@ class Plotting:
         plt.subplots_adjust(hspace=0.5)
         plt.ion()
 
-        self.nr_observations_to_plot = int(
-            util.config("forecasting", "observations_to_plot")
-        )
-        self.baseline_items = int(util.config("websocket", "baseline_items"))
-
-    def plot(self, data, average_forecasts, forecast, observation_length):
+    def plot(self, observations, average_forecasts, forecast, number_of_observations):
         """
         Args:
                 new_value (float): newest actual measured value
@@ -25,12 +18,10 @@ class Plotting:
         self.ax.clear()
 
         # Plot the data
-        self.ax.plot(data, label="Observed Value", color="blue")
+        self.ax.plot(observations, label="Observed Value", color="blue")
 
-        forecast_history_x_values = np.arange(len(average_forecasts))
         # Plot the historical average forecast
         self.ax.plot(
-            forecast_history_x_values,
             average_forecasts,
             label="Average Forecast",
             color="purple",
@@ -38,15 +29,17 @@ class Plotting:
         )
         # Plot the new value
         self.ax.scatter(
-            len(data) - 1,
-            data[-1],
+            len(observations) - 1,
+            observations[-1],
             color="red",
             label="New Value",
         )
 
         # Adjust the forecast_x_values to start from the current data point
-        forecast_x_values = np.arange(len(data) - 1, len(data) + len(forecast))
-        forecast = np.append(data[-1], forecast)
+        forecast_x_values = np.arange(
+            len(observations) - 1, len(observations) + len(forecast)
+        )
+        forecast = np.append(observations[-1], forecast)
         self.ax.plot(
             forecast_x_values,
             forecast,
@@ -64,21 +57,21 @@ class Plotting:
 
         # Update the x-axis to reflect the actual data points
         actual_x_ticks = np.arange(
-            observation_length - len(data) + 1, observation_length + 1
+            number_of_observations - len(observations) + 1, number_of_observations + 1
         )
         tick_interval = 1  # Adjust dynamically based on data length
-        self.ax.set_xticks(np.arange(0, len(data), tick_interval))
+        self.ax.set_xticks(np.arange(0, len(observations), tick_interval))
         self.ax.set_xticklabels(actual_x_ticks[::tick_interval])
 
         plt.draw()  # Update figure
         plt.pause(0.5)  # Add minor delay to plotting
 
-    def plot_error(self, errors, observation_length):
+    def plot_error(self, errors, number_of_observations):
         self.mse_ax.clear()
 
-        # Only consider the last 30 error values
-        x_values = np.arange(len(errors)) + 1
-
+        x_values = np.arange(
+            number_of_observations - len(errors) + 1, number_of_observations + 1
+        )
         self.mse_ax.plot(x_values, errors, label="Error", color="red")
 
         self.mse_ax.set_title("Absolute Error Over Time")
